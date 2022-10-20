842664c0231a19622d41af8bcff26188a99c7feb
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-10-20 10:20:19 +0100

    Updated the Order model
    Added created_timestamp and last_update_timestamp fields to the Order model.

--- src/payment/main.py
+++ src/payment/main.py
@@ -6,9 +6,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv, find_dotenv
 from starlette.requests import Request
-import requests,time
+import requests
+import time
 from fastapi.background import BackgroundTasks
 from utils import global_var
+import datetime
 
 
 env_path = Path('..')/'.env'
@@ -44,9 +46,33 @@ class Order(HashModel):
     total: float
     quantity: int
     status: str  # pending, completed, refunded
+    created_timestamp: str
+    last_update_timestamp: str
 
     class Meta:
         database = redis
+
+
+@app.get('/orders')
+def get():
+    return [format_order(pk) for pk in Order.all_pks()]
+
+
+def format_order(pk):
+
+    order = Order.get(pk)
+    return {
+        'order_id': order.pk,
+        'product_id': order.product_id,
+        'price': order.price,
+        'fee': order.quantity,
+        'total': order.quantity,
+        'quantity': order.quantity,
+        'status': 'pending',
+        'created_timestamp': order.created_timestamp,
+        'last_update_timestamp': order.last_update_timestamp
+    }
+
 
 @app.get('/orders/{pk}')
 def get(pk: str):
@@ -56,28 +82,31 @@ def get(pk: str):
 @app.post('/orders')
 async def create(request: Request, background_tasks: BackgroundTasks):
     body = await request.json()
-    #one microservice calls another using requests
+    # one microservice communicates with another using requests http library
     try:
         res = requests.get(f'http://localhost:8001/products/{body["id"]}')
     except:
         return "Unable to create order at the moment. Please contact support."
 
     product = res.json()
-    if product == global_var.PRODUCT_NOT_FOUND: 
+    if product == global_var.PRODUCT_NOT_FOUND:
         return product
 
     order = Order(
-        product_id= body['id'],
-        price= product['price'],
-        fee= 0.2*product['price'],
-        total= 1.2*product['price'],
-        quantity= product['quantity'],
-        status= 'pending'
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2*product['price'],
+        total=1.2*product['price'],
+        quantity=product['quantity'],
+        status='pending',
+        created_timestamp=str(datetime.datetime.now()),
+        last_update_timestamp=str(datetime.datetime.now())
     )
     order.save()
-    #from fastapi.background
+    # from fastapi.background
     background_tasks.add_task(order_completed, order)
     return format(order)
+
 
 def order_completed(order: Order):
     time.sleep(5)
@@ -85,7 +114,25 @@ def order_completed(order: Order):
     print(order)
     order.save()
 
-def format(order : Order):
+@app.delete('/orders')
+def get():
+    pks = Order.all_pks()
+    res_arr = []
+    print("PKS", pks)
+    if is_empty(pks):
+        return "No orders to delete!"
+    for pk in pks:
+        res = Order.delete(pk)
+        res = f'Order {pk} has been deleted' if res == 1 else 'Order {pk} not deleted'
+        res_arr.append(res)
+    return res_arr
+
+def is_empty(generator):
+    for item in generator:
+        return False
+    return True
+
+def format(order: Order):
     return {
         'order_id': order.pk,
         'product_id': order.product_id,
@@ -93,5 +140,7 @@ def format(order : Order):
         'fee': order.quantity,
         'total': order.quantity,
         'quantity': order.quantity,
-        'status': 'pending'
+        'status': 'pending',
+        'created_timestamp': order.created_timestamp,
+        'last_update_timestamp': order.last_update_timestamp
     }
