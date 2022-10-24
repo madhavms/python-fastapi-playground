ad0aa4911caa51697c49f843b18fff559ba19e54
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-10-24 22:36:00 +0100

    Load data from api and display as table

--- src/inventory-frontend/src/App.js
+++ src/inventory-frontend/src/App.js
@@ -1,18 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   useEffect(() => {
+    async function getProductDetails(){
+      const res = await fetch('http://localhost:8001/products')
+      const data = await res.json()
+      console.log(data)
+      setProducts(data)
+    }
+
+    getProductDetails()
     
   },[])
 
-
+  const [products, setProducts] = useState([])
 
   return (
     <div className="App">
-      
+    {!!products.length && 
+      <table>
+      <tbody>
+      <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Quantity</th>
+      </tr>
+      {products.map(
+        product => (
+          <tr key={product.id}>
+          <td>{product.name}</td>
+          <td>{product.price}</td>
+          <td>{product.quantity}</td>
+          </tr>
+        )
+      )}
+      </tbody>
+      </table>
+    }
     </div>
   );
 }
