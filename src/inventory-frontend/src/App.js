9fdce960237c86959ec218b90429a529d31d40c6
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-10-24 22:22:44 +0100

    Removed strict mode

--- src/inventory-frontend/src/App.js
+++ src/inventory-frontend/src/App.js
@@ -5,6 +5,12 @@ import { useEffect } from 'react';
 function App() {
 
   useEffect(() => {
+    async function getProductDetails(){
+      const data = await fetch('http://localhost:8001/products')
+      console.log(data)
+    }
+
+    getProductDetails()
     
   },[])
 
